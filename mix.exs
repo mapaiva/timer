47cfff2e698030c971a9153370d8c065aea76a28
Matheus Paiva <matheus.a.paiva@gmail.com>
2019-10-05 10:26:35 -0300

    chore(hex): prepare project for publishing
    Add docs support as well as enrich project metadata.

--- mix.exs
+++ mix.exs
@@ -1,12 +1,20 @@
 defmodule Timer.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/mapaiva/timer"
+
   def project do
     [
       app: :timer,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      name: "Timer",
+      description: "A simple time handling module",
+      source_url: @source_url,
+      package: package(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -18,6 +26,26 @@ defmodule Timer.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{
+        GitHub: @source_url
+      }
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_ref: @version,
+      source_url: @source_url,
+      extras: ["README.md"]
+    ]
   end
 end
